839500bb44ff5fb25dbc51f42dff5b07b7bab018
d4kris <kristin.luhr@gmail.com>
2012-11-05 14:34:15 -0800

    part8

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ var app = {
             //self.showAlert('Store initialized', 'All went well...');
             $('body').html(new HomeView(self.store).render().el);
         });
+        this.registerEvents();
     }, 
 
     showAlert : function (message, title) {
@@ -15,6 +16,30 @@ var app = {
             navigator.notification.alert(message, null, title, 'OK');
         } else {
             alert((title)?title + ': '+message : message);
+        }
+    }, 
+
+    registerEvents: function() {
+        var self = this;
+        // Check of browser supports touch events...
+        if (document.documentElement.hasOwnProperty('ontouchstart')) {
+            console.log('touch');
+            // ... if yes: register touch event listener to change the "selected" state of the item
+            $('body').on('touchstart', 'a', function(event) {
+                $(event.target).addClass('tappable-active');
+            });
+            $('body').on('touchend', 'a', function(event) {
+                $(event.target).removeClass('tappable-active');
+            });
+        } else {
+            console.log('mousie mousie');
+            // ... if not: register mouse events instead
+            $('body').on('mousedown', 'a', function(event) {
+                $(event.target).addClass('tappable-active');
+            });
+            $('body').on('mouseup', 'a', function(event) {
+                $(event.target).removeClass('tappable-active');
+            });
         }
     }
 

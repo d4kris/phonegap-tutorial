1c3170654ecf079bfad84cb4e5787633a2985509
d4kris <kristin.luhr@gmail.com>
2012-11-05 10:37:47 -0800

    web storage

--- js/main.js
+++ js/main.js
@@ -14,7 +14,9 @@ var app = {
     },
 
     initialize: function() {
-        this.store = new MemoryStore();
+        //this.store = new MemoryStore();
+        //this.store = new WebSqlStore();
+        this.store = new LocalStorageStore();
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
 

c28afd68b2701d6ae7f18d265dd154841c2c1de2
d4kris <kristin.luhr@gmail.com>
2012-11-05 11:18:51 -0800

    part3

--- js/main.js
+++ js/main.js
@@ -14,10 +14,21 @@ var app = {
     },
 
     initialize: function() {
+        var self = this;
         //this.store = new MemoryStore();
-        //this.store = new WebSqlStore();
-        this.store = new LocalStorageStore();
+        this.store = new WebSqlStore(function () {
+            self.showAlert('Store initialized', 'All went well...');
+        });
+        //this.store = new LocalStorageStore();
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
+    }, 
+
+    showAlert : function (message, title) {
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert((title)?title + ': '+message : message);
+        }
     }
 
 };

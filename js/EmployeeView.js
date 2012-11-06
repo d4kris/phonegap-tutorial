a7bb6da41a28347868300c13dc2247b537418d7e
d4kris <kristin.luhr@gmail.com>
2012-11-05 17:51:12 -0800

    part12

--- js/EmployeeView.js
+++ js/EmployeeView.js
@@ -3,6 +3,7 @@ var EmployeeView = function (employee) {
 	this.initialize = function () {
 		this.el = $('<div/>');
 		this.el.on('click', '.add-location-btn', this.addLocation);
+		this.el.on('click', '.change-pic-btn', this.changePicture);
 		this.el.on('click', '.add-contact-btn', this.addToContacts);
 	};
 
@@ -24,6 +25,31 @@ var EmployeeView = function (employee) {
     return false;
 	};
 
+	this.changePicture = function(event) {
+    event.preventDefault();
+    console.log('changePicture');
+    if (!navigator.camera) {
+        app.showAlert("Camera API not supported", "Error");
+        return;
+    }
+    var options =   {   quality: 50,
+                        destinationType: Camera.DestinationType.DATA_URL,
+                        sourceType: 1,      // 0:Photo Library, 1=Camera, 2=Saved Photo Album
+                        encodingType: 0     // 0=JPG 1=PNG
+                    };
+
+    navigator.camera.getPicture(
+        function(imageData) {
+            $('#image').attr('src', "data:image/jpeg;base64," + imageData);
+        },
+        function() {
+            alert('Error taking picture');
+        },
+        options);
+
+	   return false;
+	};
+
 	this.addToContacts = function(event) {
     event.preventDefault();
     console.log('addToContacts');
@@ -32,13 +58,19 @@ var EmployeeView = function (employee) {
         return;
     }
     var contact = navigator.contacts.create();
-    contact.name = {givenName: app.currentEmployee.firstName, familyName:  app.currentEmployee.lastName};
-	    var phoneNumbers = [];
-	    phoneNumbers[0] = new ContactField('work', app.currentEmployee.officePhone, false);
-	    phoneNumbers[1] = new ContactField('mobile', app.currentEmployee.cellPhone, true); // preferred number
-	    contact.phoneNumbers = phoneNumbers;
-	    contact.save();
-	    return false;
+    contact.name = {
+    	givenName: app.currentEmployee.firstName, 
+    	familyName:  app.currentEmployee.lastName
+    };
+	  var phoneNumbers = [];
+    phoneNumbers[0] = new ContactField('work', app.currentEmployee.officePhone, false);
+    phoneNumbers[1] = new ContactField('mobile', app.currentEmployee.cellPhone, true); // preferred number
+    contact.phoneNumbers = phoneNumbers;
+    contact.save(
+    	function() {app.showAlert('save success')},
+    	function(error) {app.showAlert('save failed: '+error.code)}
+    );
+    return false;
 	};
 
 	this.initialize();
